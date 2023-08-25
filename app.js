84f0b45206541715d9c41290dc49676fa90b6039
leoxP <patrabaibhab1@gmail.com>
2023-08-25 18:31:05 +0530

    prototype

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded',()=>{
+
     const bird=document.querySelector('.bird');
     const gameDisplay=document.querySelector('.game-container');
     const ground=document.querySelector('.ground');
@@ -6,6 +7,8 @@ document.addEventListener('DOMContentLoaded',()=>{
     let birdLeft=220;
     let birdBottom=100;
     let gravity=2;
+    let isGameOver=false;
+    let gap=430;
 
     function startGame(){
         birdBottom-=gravity;
@@ -13,7 +16,8 @@ document.addEventListener('DOMContentLoaded',()=>{
         bird.style.left=birdLeft+'px';
         //adds a 100px to bottom in css file
     }
-    let timerId=setInterval(startGame,20);
+
+    let gametimerId=setInterval(startGame,20);
     //every 20 ms startGame is invoked
 
     function control(e){
@@ -27,19 +31,60 @@ document.addEventListener('DOMContentLoaded',()=>{
         if(birdBottom<500) birdBottom+=50;
         bird.style.bottom-=birdBottom+'px';
     }
-    document.addEventListener('keyup',control);
 
-    function generatedObstacle(){
-        let obstacleLeft=500;
-        let obstacleBottom=150;
-        const obstacle=document.createElement('div');
-        obstacle.classList.add('obstacle');
-        gameDisplay.appendChild(obstacle);
-        obstacle.style.left=obstacleLeft+'px';
-        obstacle.style.bottom=obstacleBottom+'px';
+    function gameOver(){
+        clearInterval(gametimerId);
+        isGameOver=true;
+        document.removeEventListener('keyup',control);
     }
 
-    generatedObstacle();
+    document.addEventListener('keyup',control);
 
-    //clearInterval(timerId);
+    function generateObstacle(){
+        let obstacleLeft=500;
+        let randomHeight=Math.random()*60;
+        let obstacleBottom=randomHeight;
+        const obstacle=document.createElement('div');
+        const topObstacle=document.createElement('div');
+
+        if(!isGameOver){ 
+            obstacle.classList.add('obstacle');
+            topObstacle.classList.add('topObstacle');
+        }
+
+        gameDisplay.appendChild(obstacle);
+        gameDisplay.appendChild(topObstacle);
+
+        obstacle.style.left=obstacleLeft+'px';
+        topObstacle.style.left=obstacleLeft+'px';
+        obstacle.style.bottom=obstacleBottom+'px';
+        topObstacle.style.bottom=obstacleBottom+gap+'px';
+
+        function moveObstacle(){
+            obstacleLeft-=2;
+            obstacle.style.left=obstacleLeft+'px';
+            topObstacle.style.left=obstacleLeft+'px';
+
+
+            if(obstacleLeft=== -60){ //obstacle width=60
+                clearInterval(timerId);
+                gameDisplay.removeChild(obstacle);
+                gameDisplay.removeChild(topObstacle);
+            }
+            if(
+                obstacleLeft>200 && obstacleLeft<280 && birdLeft===220 && (birdBottom+150<=obstacleBottom+300 || birdBottom>=obstacleBottom+gap-195)||
+                birdBottom===0
+                ){
+                gameOver();
+                clearInterval(timerId); 
+                //checking for collision around the center of the grid
+            }
+        }
+        let timerId=setInterval(moveObstacle,20);
+
+        //generating new obstacles every 3 seconds
+        if(!isGameOver) setTimeout(generateObstacle,5000); 
+    }
+
+    generateObstacle();
 })
